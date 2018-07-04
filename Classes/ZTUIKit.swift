2a649dd694621d17fc8b1ef758e4947a7de311cb
say_goodbye <say_goodbye000@163.com>
2018-07-04 14:41:46 +0800

    0.0.8
    修修改改，缝缝补补

--- Classes/ZTUIKit.swift
+++ Classes/ZTUIKit.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @objc public class ZTUIKit: NSObject {
-
+    
     /// label设置
     ///
     /// - Parameters: 设置参数
@@ -19,7 +19,7 @@ import UIKit
     ///   - textfont: textfont
     ///   - textAlignment: Alignment
     /// - Returns: label
-   @objc public class func createLabel(frame:CGRect, text:String, textColor:UIColor, textfont:UIFont, textAlignment:NSTextAlignment)-> UILabel{
+    @objc public class func createLabel(frame:CGRect, text:String, textColor:UIColor, textfont:UIFont, textAlignment:NSTextAlignment)-> UILabel{
         
         let color:UIColor?  = textColor
         
@@ -31,13 +31,36 @@ import UIKit
         
         return label
     }
-     
     
+    /// 富文本
+    ///
+    /// - Parameters:
+    ///   - text: 文本内容
+    ///   - line: 换行间距
+    ///   - loc: 第几个字符开始替换
+    ///   - len: 替换长度
+    ///   - color: 替换颜色
+    ///   - font: 替换字号
+    ///   - alignment: 对齐方式， 0:居左 1：居中 2：居右
+    /// - Returns: AttributedString
+    public class func swiftAttributedString(text:String, line:CGFloat, loc:Int, len:Int, color:UIColor, font:UIFont, alignment:Int) ->NSMutableAttributedString{
+        
+        let paragraphStyle:NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = line
+        paragraphStyle.alignment   = NSTextAlignment(rawValue: alignment)!//
+        
+        let attributedStr:NSMutableAttributedString = NSMutableAttributedString.init(string: text)
+        attributedStr.addAttribute(NSAttributedStringKey.foregroundColor, value: color, range: NSMakeRange(loc, len))
+        attributedStr.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, text.count))
+        attributedStr.addAttribute(NSAttributedStringKey.font, value: font, range: NSMakeRange(loc, len))
+        
+        return attributedStr
+    }
     
     /// 随机色
     ///
     /// - Returns: UIColor
-   @objc public class func randomColor() -> UIColor{
+    @objc public class func randomColor() -> UIColor{
         
         let hueRandom:CGFloat   = CGFloat(arc4random() % 256)
         let colorRandom:CGFloat = CGFloat(arc4random() % 128)
